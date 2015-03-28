ff884bf0d88403526feb78147dcd1fa716e071a2
hsieh jing <xqj198404@gmail.com>
2015-03-29 00:07:00 +0800

    20150329

---
+++ netWebHtml/blog/App_Code/Brower.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blog.App_Code
+{
+    public static class Brower
+    {
+        public static bool isLowBrower()
+        {
+            return false;
+        }
+    }
+}
---
+++ netWebHtml/blog/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List() {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
